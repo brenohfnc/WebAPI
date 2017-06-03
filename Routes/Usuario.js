fb2d0f7fd48a29fe83faca64ed42eb4189822443
Breno <brenohfnc@gmail.com>
2017-06-03 16:45:09 -0300

    1.1

--- Routes/Usuario.js
+++ Routes/Usuario.js
@@ -1,5 +1,5 @@
 var mongo = require('mongodb');
-
+var ObjectId = require('mongodb').ObjectID;
 var Server = mongo.Server, Db = mongo.Db, BSON = mongo.BSONPure;
 
 var server = new Server('localhost', 27017, { auto_reconnect: true });
@@ -31,7 +31,7 @@ exports.RealizarLogin = function (req, res) {
 
 exports.CadastrarUsuario = function (req, res) {
     var usuario = req.body;
-    console.log('Inserindo usuário: ' + JSON.stringify(usuario));
+
     db.collection('Usuario', function (err, collection) {
         collection.findOne({ 'CpfCnpj': usuario.CpfCnpj }, function (err, item) {
             if (item != null) {
@@ -41,8 +41,12 @@ exports.CadastrarUsuario = function (req, res) {
                     if (err) {
                         res.status(500).json({ ErrorMessage: 'Ocorreu um erro ao inserir usuário' });
                     } else {
-                        console.log('Sucesso ao inserir usuário: ' + JSON.stringify(result[0]));
-                        res.status(200).json({ Id: result._id, Nome: result.Nome, Perfil: result.Perfil });
+                         collection.findOne({ 'CpfCnpj': usuario.CpfCnpj }, function (err, item2) {
+                             if (err) {
+                                res.status(500).json({ ErrorMessage: 'Ocorreu um erro ao inserir usuário' });
+                             }
+                            res.status(200).json({ Id: item2._id, Nome: item2.Nome, Perfil: item2.Perfil });
+                         });
                     }
                 });
             }
@@ -54,7 +58,7 @@ exports.ListarDoacoes = function (req, res) {
     var id = req.query.id;
 
     db.collection('Usuario', function (err, collection) {
-        collection.findOne({ '_id': id }, function (err, item) {
+        collection.findOne({ '_id': new ObjectId(id) }, function (err, item) {
             if (item != null) {
                 if (item.Perfil[0].hasOwnProperty("Associado")) {
                     res.status(200).json({ ListaDoacoes: item.DoacoesRealizadas });
@@ -74,13 +78,13 @@ exports.RealizarDoacao = function (req, res) {
     var objDoacao = req.body;
 
     db.collection('Usuario', function (err, collection) {
-        collection.findOne({ '_id': idDoador }, function (err, item) {
+        collection.findOne({ '_id': new ObjectId(idDoador) }, function (err, item) {
             if (item != null) {
                 item.DoacoesRealizadas.push(objDoacao);
-                collection.update({ '_id': item._id }, item, function (err, result) {
+                collection.update({ '_id': new ObjectId(item._id) }, item, function (err, result) {
                     collection.findOne({ 'CpfCnpj': cpfCnpjAssistido }, function (err, item2) {
                         item2.DoacoesRecebidas.push({ Data: objDoacao.Data, Valor: objDoacao.Valor });
-                        collection.update({ '_id': item2._id }, item2, function (err, result) {
+                        collection.update({ '_id': new ObjectId(item2._id) }, item2, function (err, result) {
                             res.status(200).json({ SuccessMessage: 'Doação realizada com sucesso.' });
                         });
                     });
@@ -97,9 +101,10 @@ exports.ListarUsuarios = function (req, res) {
 
     if (perfil == 'Associado') {
         db.collection('Usuario', function (err, collection) {
-            collection.find({ 'Perfil': [{ 'Associado': true }] }, function (err, itens) {
+            collection.find({ 'Perfil': [{ 'Associado': true }] }).toArray(function (err, itens) {
                 if (itens != null) {
-                    res.status(200).json(itens);
+                    delete itens["Senha"];
+                    res.status(200).json({ ListaUsuarios: itens});
                 } else {
                     res.status(400).json({ ErrorMessage: 'Usuários não encontrados' });
                 }
@@ -107,9 +112,10 @@ exports.ListarUsuarios = function (req, res) {
         });
     } else {
         db.collection('Usuario', function (err, collection) {
-            collection.find({ 'Perfil': [{ 'Assistido': true }] }, function (err, itens) {
+            collection.find({ 'Perfil': [{ 'Assistido': true }] }).toArray(function (err, itens) {
                 if (itens != null) {
-                    res.status(200).json(itens);
+                    delete itens["Senha"];
+                    res.status(200).json({ ListaUsuarios: itens});
                 } else {
                     res.status(400).json({ ErrorMessage: 'Usuários não encontrados' });
                 }
--- server.js
+++ server.js
@@ -8,8 +8,25 @@ var UsuarioController = require('./Routes/Usuario');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 app.post('/Usuario/Login', UsuarioController.RealizarLogin)
-app.post('/Usuario/Cadastrar', UsuarioController.CadastrarUsuario)
+app.post('/Usuario/Cadastro', UsuarioController.CadastrarUsuario)
 
 app.post('/Usuario/RealizarDoacao', UsuarioController.RealizarDoacao)
 app.get('/Usuario/ListarDoacoes', UsuarioController.ListarDoacoes)
